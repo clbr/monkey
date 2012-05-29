d6779f7208308ac81b20373ff4554107bc09f9ac
Lauri Kasanen <cand@gmx.com>
2012-05-29 19:32:42 +0300

    My (C)

--- src/mk_lib.c
+++ src/mk_lib.c
@@ -2,7 +2,7 @@
 
 /* Monkey HTTP Daemon
  * ------------------
- * Copyright (C) 2001-2012, Eduardo Silva P. <edsiper@gmail.com>
+ * Copyright (C) 2012, Lauri Kasanen <cand@gmx.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
