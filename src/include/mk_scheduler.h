cd450a46d285a02cf557c9469e21d8c79a54a1b5
Lauri Kasanen <cand@gmx.com>
2012-06-17 17:18:46 +0300

    sched: Mark mk_sched_register_thread as static

--- src/include/mk_scheduler.h
+++ src/include/mk_scheduler.h
@@ -83,7 +83,6 @@ pthread_key_t worker_sched_node;
 extern pthread_mutex_t mutex_worker_init;
 
 void mk_sched_init();
-int mk_sched_register_thread(int epoll_fd);
 int mk_sched_launch_thread(int max_events, pthread_t *tout, mklib_ctx ctx);
 void *mk_sched_launch_epoll_loop(void *thread_conf);
 struct sched_list_node *mk_sched_get_handler_owner(void);
